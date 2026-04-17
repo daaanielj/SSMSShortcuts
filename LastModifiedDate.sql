ae3d90da11844850d6875b7d383eefb9dad0c835
daaanielj <daaanielj@outlook.com>
2026-04-16 19:22:23 -0700

    Update LastModifiedDate.sql

--- LastModifiedDate.sql
+++ LastModifiedDate.sql
@@ -1,8 +1,25 @@
-CREATE PROCEDURE SelectByLastModified
+CREATE OR ALTER PROCEDURE SelectByLastModified
     @TableName NVARCHAR(128)
 AS
 BEGIN
-    DECLARE @sql NVARCHAR(MAX)
-    SET @sql = 'SELECT * FROM ' + QUOTENAME(@TableName) + ' ORDER BY LastModifiedDate DESC;'
-    EXEC sp_executesql @sql
+    DECLARE @sql NVARCHAR(MAX);
+    DECLARE @OrderColumn NVARCHAR(128);
+
+    IF EXISTS (
+        SELECT 1
+        FROM sys.columns
+        WHERE object_id = OBJECT_ID(@TableName)
+          AND name = 'LastModifiedDate'
+    )
+        SET @OrderColumn = 'LastModifiedDate';
+    ELSE
+        SET @OrderColumn = 'Id';
+
+    SET @sql = '
+        SELECT *
+        FROM ' + QUOTENAME(@TableName) + '
+        ORDER BY ' + QUOTENAME(@OrderColumn) + ' DESC;
+    ';
+
+    EXEC sp_executesql @sql;
 END
